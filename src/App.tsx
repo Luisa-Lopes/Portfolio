2427d9dc954fdcfdc37de7e8c56500bdb21d5e14
Ana <aninhaslopess@gmail.com>
2026-08-31 21:18:08 -0300

    fix

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Portfolio from "./pages/Portifolio/Portifolio";
+import Game from "./pages/Game/Game";
+
+function App() {
+  return (
+    <BrowserRouter basename="/Portfolio">
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
